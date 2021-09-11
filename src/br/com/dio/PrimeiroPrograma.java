0327ec8d8398e2270b1863b6c5d0f92289967905
vertinho0 <ewertonlipesd@gmail.com>
2021-09-11 17:27:09 -0300

    firts commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,17 @@
+package br.com.dio;
+
+import br.com.dio.model.gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        gato gato = new gato();
+
+        System.out.println(gato);
+
+        /* int a = 5;
+        int b = 3;
+
+        System.out.println("hello world " + (a+b));*/
+    }
+}
